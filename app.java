b580994bac7081513b3dea9ee0c332e2a8f5edb8
Liteuszek <122406153+Liteuszek@users.noreply.github.com>
2023-06-17 21:54:52 +0200

    Update app.java

--- app.java
+++ app.java
@@ -1,18 +1,37 @@
 package com.company.model;
 
+/**
+ * Reprezentuje salę konferencyjną.
+ */
 public class Room {
     private String name;
     private int capacity;
 
+    /**
+     * Tworzy nową salę konferencyjną.
+     *
+     * @param name     nazwa sali
+     * @param capacity pojemność sali
+     */
     public Room(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
     }
 
+    /**
+     * Zwraca nazwę sali.
+     *
+     * @return nazwa sali
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Zwraca pojemność sali.
+     *
+     * @return pojemność sali
+     */
     public int getCapacity() {
         return capacity;
     }
